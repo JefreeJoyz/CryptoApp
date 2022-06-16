1a5d5d1704bb1f749f6277af3ee49453651a5553
JefreeJoyz <jefpad12@gmail.com>
2022-06-16 10:03:45 +0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Eugene Yakushev on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
