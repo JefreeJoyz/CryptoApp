bb39a32ea8f2ad1e28d24be19ab13f6e5b14da82
JefreeJoyz <jefpad12@gmail.com>
2022-06-28 15:40:42 +0300

    Downloaded market data for statistics

--- Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,18 +10,14 @@ import Combine
 
 class HomeViewModel: ObservableObject {
     
-    @Published var statistics: [StatisticModel] = [
-        StatisticModel(title: "title", value: "value", percentageChange: 1),
-        StatisticModel(title: "title", value: "value"),
-        StatisticModel(title: "title", value: "value"),
-        StatisticModel(title: "title", value: "value", percentageChange: -5),
-        ]
+    @Published var statistics: [StatisticModel] = []
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDatService()
     private var cancellables = Set<AnyCancellable>()
     
     init () {
@@ -31,13 +27,21 @@ class HomeViewModel: ObservableObject {
     func addSubscribers () {
         
         $searchText
-            .combineLatest(dataService.$allCoins) // теперь мы подписаны на $searchText и $allCoins
+            .combineLatest(coinDataService.$allCoins) // теперь мы подписаны на $searchText и $allCoins
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        
+        marketDataService.$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] returnedStats in
+                self?.statistics = returnedStats
+            }
+            .store(in: &cancellables)
+
     }
     
     private func filterCoins (text: String, coins: [CoinModel]) -> [CoinModel] {
@@ -50,5 +54,25 @@ class HomeViewModel: ObservableObject {
             coin.symbol.lowercased().contains(lowercasedText) ||
             coin.id.lowercased().contains(lowercasedText)
         }
+    }
+    
+    private func mapGlobalMarketData (marketDataModel: MarketDataModel?) -> [StatisticModel] {
+        var stats: [StatisticModel] = []
+        guard let data = marketDataModel else { return stats }
+       let marketCap = StatisticModel(title: "marketCap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+       stats.append(marketCap)
+       
+       let volume = StatisticModel(title: "24h volume", value: data.volume)
+       stats.append(volume)
+       let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+       stats.append(btcDominance)
+       let portfolio = StatisticModel(title: "Portfolio value", value: "$0.00", percentageChange: 0)
+       stats.append(portfolio)
+       
+       // Альтернативная запись поштучного добавления
+//                stats.append([
+//                marketCap, volume, btcDominance, portfolio
+//                ])
+       return stats
     }
 }
---
+++ Crypto/Services/MarketDatService.swift
@@ -0,0 +1,29 @@
+//
+//  MarketDatService.swift
+//  Crypto
+//
+//  Created by Eugene Yakushev on 28.06.2022.
+//
+
+import Foundation
+import Combine
+
+class MarketDatService {
+    @Published var marketData: MarketDataModel? = nil
+    var marketDataSubscription: AnyCancellable?
+    
+    init () {
+        getData ()
+    }
+    
+    private func getData () {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self , decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedGlobalData in
+                self?.marketData = returnedGlobalData.data // .data ? 
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+}
